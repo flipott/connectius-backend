045091bfd518c4646accd4eec60545f329080432
flipott <ottphillipa@gmail.com>
2023-02-03 22:49:55 -0500

    Create user model

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    profilePicture: { data: Buffer, contentType: String },
+    posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+    connections: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    liked: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+    created: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("User", UserSchema);
