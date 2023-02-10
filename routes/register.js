04fa0b2197ff1a7f1ceffc842acaee1177332866
flipott <ottphillipa@gmail.com>
2023-02-09 19:37:03 -0500

    Sanitize input from Register form

--- routes/register.js
+++ routes/register.js
@@ -2,21 +2,28 @@ const User = require("../models/user");
 const router = require("express").Router();
 const jwt = require("jsonwebtoken");
 const auth = require("../middleware/auth");
+const { check, body, sanitize } = require("express-validator");
 require('dotenv').config()
 
 // Create a user
 router.post("/", (req, res, next) => {
-    const user = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password,
-    }).save((err, results) => {
-        if (err) {
-            return next(err);
-        }
-        res.json(results);
-    });
+    if (req.body.password !== req.body["confirm-password"]) {
+        return res.status(401).json({error: "Passwords do not match"});
+    }
+
+    console.log(req.body);
+    res.json("Works")
+    // const user = new User({
+    //     firstName: req.body.firstName,
+    //     lastName: req.body.lastName,
+    //     email: req.body.email,
+    //     password: req.body.password,
+    // }).save((err, results) => {
+    //     if (err) {
+    //         return next(err);
+    //     }
+    //     res.json(results);
+    // });
 });
 
 module.exports = router;
