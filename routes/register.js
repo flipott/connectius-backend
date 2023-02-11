1e446d05d080623f8f0ee4b164810d35a2e2dae9
flipott <ottphillipa@gmail.com>
2023-02-10 21:55:34 -0500

    Implement sanitation and validation for registration

--- routes/register.js
+++ routes/register.js
@@ -2,28 +2,59 @@ const User = require("../models/user");
 const router = require("express").Router();
 const jwt = require("jsonwebtoken");
 const auth = require("../middleware/auth");
-const { check, body, sanitize } = require("express-validator");
+const { body, validationResult } = require("express-validator");
 require('dotenv').config()
 
 // Create a user
-router.post("/", (req, res, next) => {
-    if (req.body.password !== req.body["confirm-password"]) {
-        return res.status(401).json({error: "Passwords do not match"});
-    }
+router.post(
+    "/",
+    body("first-name").isLength({min: 2}).trim().escape()
+    .withMessage("First name must be at least 2 characters long"),
 
-    console.log(req.body);
-    res.json("Works")
-    // const user = new User({
-    //     firstName: req.body.firstName,
-    //     lastName: req.body.lastName,
-    //     email: req.body.email,
-    //     password: req.body.password,
-    // }).save((err, results) => {
-    //     if (err) {
-    //         return next(err);
-    //     }
-    //     res.json(results);
-    // });
-});
+    body("last-name").isLength({min: 2}).trim().escape()
+    .withMessage("Last name must be at least 2 characters long"),
+
+    body("email").isEmail().normalizeEmail()
+    .withMessage("Email address must be a valid email"),
+
+    body("password").isLength({min: 3})
+    .withMessage("Password must be at least 3 characters long"),
+
+    body("confirm-password").custom((value, {req}) => {
+        if (value !== req.body.password) {
+            throw new Error("Passwords do not match");
+        }
+        return true;
+    }),
+    (req, res, next) => {
+        console.log(req.body);
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array()}); 
+        } 
+
+        User.find({ email: req.body.email }, (err, results) => {
+            if (err) {
+                return next(err);
+            }
+            if (!results.length) {
+                const user = new User({
+                    firstName: req.body["first-name"],
+                    lastName: req.body["last-name"],
+                    email: req.body.email,
+                    password: req.body.password,
+                }).save((err, results) => {
+                    if (err) {
+                        return next(err);
+                    }
+                    res.json(results);
+                });
+            } else {
+                return res.status(400).json({ errors: [{"msg": "Account already exists"}]});
+            }
+        });
+    }
+);
+
 
 module.exports = router;
