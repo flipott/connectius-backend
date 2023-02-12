8f158f4791e619a480cd94e061c634021361ca05
flipott <ottphillipa@gmail.com>
2023-02-12 00:32:55 -0500

    Handle login

---
+++ routes/login.js
@@ -0,0 +1,41 @@
+const User = require("../models/user");
+const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+const verifyUser = require("../middleware/verifyUser");
+const bcrypt = require("bcryptjs");
+const { body, validationResult } = require("express-validator");
+require('dotenv').config();
+
+router.post("/", (req, res, next) => {
+    User.find({ email: req.body.email }, (err, results) => {
+        if (err) {
+            return next(err);
+        }
+        if (!results.length) {
+            // User does not exist
+            return res.status(400).json({ errors: [{"msg": "Incorrect email address"}]})
+        } else {
+            const user = { id: results[0]._id };
+            // User exists
+            bcrypt.compare(req.body.password, results[0].password, (err, pasRes) => {
+                if (err) {
+                    return next(err);
+                }
+                if (pasRes) {
+                    // Passwords match
+                    const token = jwt.sign({ user }, process.env.SECRET, { expiresIn: "30s" });
+                    res.json({
+                        token: token
+                    });
+                } else {
+                    // Passwords do not match
+                    return res.status(400).json({ errors: [{"msg": "Incorrect password"}]});
+                }
+            });
+        }
+    });
+});
+
+
+
+module.exports = router;
