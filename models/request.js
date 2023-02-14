8caca4a9c4dbc3af33d554baf959c8d18a3627d3
flipott <ottphillipa@gmail.com>
2023-02-14 00:17:01 -0500

    Create Connection and Request schemas

---
+++ models/request.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const RequestSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+});
+
+module.exports = mongoose.model("Request", RequestSchema);
