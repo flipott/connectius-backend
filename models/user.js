0b1a69d20e45630a32b82febfa64f700cb823375
flipott <ottphillipa@gmail.com>
2023-02-22 19:34:39 -0500

    Refactor profilePicture to use a default image

--- models/user.js
+++ models/user.js
@@ -1,12 +1,26 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const fs = require("fs");
+
+const defaultProfilePicturePath = __dirname + '/usertemp.png';
+const defaultProfilePictureType = 'image/png';
+
 
 const UserSchema = new Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    profilePicture: { data: Buffer, contentType: String },
+    profilePicture: {
+        data: {
+          type: Buffer,
+          default: fs.readFileSync(defaultProfilePicturePath)
+        },
+        contentType: {
+          type: String,
+          default: defaultProfilePictureType
+        }
+      },
     posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
     connections: [{ type: Schema.Types.ObjectId, ref: "User" }],
     requests: [{ type: Schema.Types.ObjectId, ref: "User" }],
