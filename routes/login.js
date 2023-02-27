b90f31743cc98880d7194071785c9a8c6c3f4a9c
flipott <ottphillipa@gmail.com>
2023-02-27 18:26:47 -0500

    Change login time

--- routes/login.js
+++ routes/login.js
@@ -23,7 +23,7 @@ router.post("/", (req, res, next) => {
                 }
                 if (pasRes) {
                     // Passwords match
-                    const token = jwt.sign({ user }, process.env.SECRET, { expiresIn: "3m" });
+                    const token = jwt.sign({ user }, process.env.SECRET, { expiresIn: "10m" });
                     res.json({
                         token: token,
                         user: user.id
